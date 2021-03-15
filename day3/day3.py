dac51eb3c44593af012b2ad708eca4eb5ac829ba
Adam <75036521+Adam0J@users.noreply.github.com>
2021-03-15 15:11:37 +0000

    part2 complete.

--- day3/day3.py
+++ day3/day3.py
@@ -4,18 +4,23 @@ with open("input.txt","r") as f:
         array.append(line.replace('\n',''))
 
 
-def tree_on_route(array):
+def tree_on_route(array, right_num, down_num):
     down = 0
     right = 0
     count = 0
-    while down != len(array):
+    while down < len(array):
         right_mod = right % 31
-        if array[down][right_mod] == '#':
+        down_mod = down % 323
+        if array[down_mod][right_mod] == '#':
             count += 1
-        right += 3
-        down += 1
+        right += right_num
+        down += down_num
     return count
 
 
-a = tree_on_route(array)
-print(a)
+a = tree_on_route(array, 1, 1)
+b = tree_on_route(array, 3, 1)
+c = tree_on_route(array, 5, 1)
+d = tree_on_route(array, 7, 1)
+e = tree_on_route(array, 1, 2)
+print(a*b*c*d*e)
