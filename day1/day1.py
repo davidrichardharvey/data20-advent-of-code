ae2dbf919168ab7ecdf87096bd2319a487d54ad0
Mark Wu <m_wu@hotmail.com>
2021-03-15 12:06:23 +0000

    code solution for day 1 - updated and finished

--- day1/day1.py
+++ day1/day1.py
@@ -1,16 +1,34 @@
-with open('day1input.txt', 'r') as f:
+import numpy as np
 
+# Open the file of inputs
+with open('..\day1\day1input.txt', 'r') as f:
+    # turn the text file into a sorted list
     nums = sorted([int(i.rstrip('\n')) for i in f.readlines()])
 
+answers = []
 
+
+# function for getting 2 numbers that sum to specific target
 def sum_components(target):
     for i in nums:
         if target - i in nums:
-            print(i, target - i)
-            print(i * (target - i))
+            answers.extend([i, target - i])
             break
         if i >= target / 2:
             break
+    return answers
 
 
-sum_components(2020)
+# function for getting 3 numbers that sum to specific target using sum_components
+def three_sum_components(target):
+    temp = [i for i in nums if i < target//3]
+    for j in temp:
+        rem = target - j
+        if len(sum_components(rem)) > 0:
+            answers.append(j)
+            break
+
+
+three_sum_components(2020)
+print(answers)
+print(np.prod(answers))
