9860d2c8be9001c91b62608e2eab72f14cbf7292
Reece Penn <reecepenn97@gmail.com>
2021-03-15 15:15:02 +0000

    AoC day two solutions (part one and two)

--- day2/day2.py
+++ day2/day2.py
@@ -1,5 +1,7 @@
-# Notes are for my benefit
-def verify_password(line):
+# Notes are for my benefit:
+
+
+def verify_password_part_one(line):
     # separates list entry into required policy and password via ':'
     required, password = [item.strip() for item in line.split(':')]
     # bounds are found in the initial string, bar the last two index values
@@ -17,19 +19,45 @@ def verify_password(line):
         return False
 
 
-def count_password(password_list):
+def count_password_part_one(password_list):
     good_passwords = 0
     for line in password_list:
-        if verify_password(line):
+        if verify_password_part_one(line):
             good_passwords += 1
-    print(f"Number of acceptable passwords:  {good_passwords}")
+    print(f"Number of acceptable passwords for part one:  {good_passwords}")
     return good_passwords
+
+
+def verify_password_part_two(line):
+    required, password = [item.strip() for item in line.split(':')]
+    bounds = required[:-2]
+    letter = required[-1]
+    index1, index2 = [int(item) for item in bounds.split('-')]
+    new_index1 = (int(index1) - 1)
+    new_index2 = (int(index2) - 1)
+    letter_index1 = password[new_index1]
+    letter_index2 = password[new_index2]
+
+    if letter_index1 == letter and letter_index2 != letter:
+        return True
+    elif letter_index1 != letter and letter_index2 == letter:
+        return True
+    else:
+        return False
+
+
+def count_password_part_two(password_list):
+    valid_passwords = 0
+    for line in password_list:
+        if verify_password_part_two(line):
+            valid_passwords += 1
+    print(f"Number of acceptable passwords for part two:  {valid_passwords}")
+    return valid_passwords
 
 
 password_list = []
 with open("day2input.txt", "r") as f:
     for line in f:
         password_list.append(line.strip())
-    count_password(password_list)
-
-
+    count_password_part_one(password_list)
+    count_password_part_two(password_list)
