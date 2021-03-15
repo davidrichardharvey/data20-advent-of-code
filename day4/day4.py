f1d1be8f9ebfee6bb828784df3597bf6c1eba82b
Adam <75036521+Adam0J@users.noreply.github.com>
2021-03-15 21:56:47 +0000

    Working part1, added if statement to end of function so last passport not missed.

--- day4/day4.py
+++ day4/day4.py
@@ -3,24 +3,24 @@ with open("input.txt") as fp:
 
 
 def check_passports(lines):
-    valid_passports = []
     all_keys = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
     count = 0
     temp = {}
     for line in lines:
-
         if line == '':
-            print(list(temp.keys()))
             if all(keys in list(temp.keys()) for keys in all_keys):
                 count += 1
             temp = {}
-            pass
 
         else:
-
             new_line = line.split(' ')
+            print(new_line)
             for i in new_line:
                 temp[i.split(':')[0]] = i.split(':')[1]
+
+    # Was missing check on last passport due to the if/else statement layout
+    if all(keys in list(temp.keys()) for keys in all_keys):
+        count += 1
 
     return count
 
