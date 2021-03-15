b0645e5f5ca58320de9c9c48056c7793fc2bb554
Mark Wu <m_wu@hotmail.com>
2021-03-15 11:39:52 +0000

    code solution for day 1

---
+++ day1/day1.py
@@ -0,0 +1,15 @@
+with open('day1input.txt', 'r') as f:
+
+    nums = sorted([int(i.rstrip('\n')) for i in f.readlines()])
+
+
+def sum_components(target):
+    for i in nums:
+        if target - i in nums:
+            print(i, target - i)
+            break
+        if i >= target / 2:
+            break
+
+
+sum_components(2020)
