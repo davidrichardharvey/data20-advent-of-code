702ab0c1d714a8fabbecf235acb1a2093a2e1425
M-Whit <matthew.whitaker1997@yahoo.co.uk>
2021-03-15 14:06:24 +0000

    matt - Day 2 Complete!

--- day2/day2.py
+++ day2/day2.py
@@ -3,6 +3,7 @@ with open("day2input.txt", "r") as f:
 
 temp = []
 valid = 0
+new_valid = 0
 
 # strip and replace unnecessary characters
 for entry in content:
@@ -32,4 +33,22 @@ for password in temp:
         valid += 1
 
 print(f"\nThere are {valid} valid passwords in the list")
+
+
+# Part 2
+print("\n\n")
+
+# Generating new Python indexes
+for i in temp:
+    i[0][0] = i[0][0] - 1
+    i[0][1] = i[0][1] - 1
+print(temp)
+
+# Comparing indexes using XOR operator
+for i in temp:
+    lower = i[0][0]
+    upper = i[0][1]
+    if bool(i[2][lower] == i[1]) != bool(i[2][upper] == i[1]):
+        new_valid += 1
+print(f"Actually, there are {new_valid} valid passwords in the list")
 
