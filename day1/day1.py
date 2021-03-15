13ba732bd9714dfc423d599fc40d1536bcf060d4
Adam <75036521+Adam0J@users.noreply.github.com>
2021-03-15 12:10:31 +0000

    part2 complete.

--- day1/day1.py
+++ day1/day1.py
@@ -4,11 +4,26 @@ with open("day1input.txt","r") as f:
     for line in f: # read rest of lines
         array.append([int(x.replace('\n','')) for x in line.split()])
 
-# Because of how i opened the file, had to index each number in the array
-for i in array:
-    for j in array:
-        if int(i[0]) != int(j[0]):
-            if i[0] + j[0] == 2020:
-                answer = (i[0] * j[0])
+def part1():
+    # Because of how i opened the file, had to index each number in the array
+    for i in array:
+        for j in array:
+            if int(i[0]) != int(j[0]):
+                if i[0] + j[0] == 2020:
+                    answer = (i[0] * j[0])
+    return answer
 
-print(answer)
+
+def part2():
+    # Because of how i opened the file, had to index each number in the array
+    for i in array[0:100]:
+        for j in array[100:150]:
+            for k in array[150:200]:
+                    if i[0] + j[0] + k[0] == 2020:
+                        answer = (i[0] * j[0] * k[0])
+                        break
+
+                    else:
+                        continue
+
+    return answer
