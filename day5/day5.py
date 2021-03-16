84bfa6d69db6e5b18cdb1030492c2b1c31bbcd8c
Adam <75036521+Adam0J@users.noreply.github.com>
2021-03-16 10:38:00 +0000

    day5 part 2 working solution.

--- day5/day5.py
+++ day5/day5.py
@@ -6,7 +6,7 @@ with open("input.txt","r") as f:
 
 def highest_boarding_pass(array):
     max_id = 0
-
+    all_seat_ids = []
     # for each boarding pass in array the following code is executed.
     for bpass in array:
 
@@ -37,8 +37,17 @@ def highest_boarding_pass(array):
         if temp > max_id:
             max_id = temp
 
-    return max_id
+    # part 2 of question, saves all seat id's, sorts the list find when they is a missing number in
+    # sequence as we know this is where our seat will be.
+        all_seat_ids.append(temp)
+    all_seat_ids.sort()
+    for i in range(1, len(all_seat_ids)):
+        if all_seat_ids[i] - all_seat_ids[i-1] == 2:
+            free_seat = all_seat_ids[i] - 1
+
+    return max_id, free_seat
 
 
 a = highest_boarding_pass(array)
-print(a)
+print(a[0])
+print(a[1])
