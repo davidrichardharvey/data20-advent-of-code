c6cdfb7c08ddf565fc4b0f754ace62d023dbfd3c
M-Whit <matthew.whitaker1997@yahoo.co.uk>
2021-03-16 10:17:22 +0000

    matt- Day 3 Complete!

--- day3/day3.py
+++ day3/day3.py
@@ -4,7 +4,7 @@ with open("day3input.txt", "r") as f:
 content = content.split("\n")
 
 for i in range(len(content)):
-    content[i] = content[i] * 50
+    content[i] = content[i] * 200
 
 # content[0] provides first line
 # content[0][4] provides first line, fifth item
@@ -22,7 +22,50 @@ for i in range(len(content)):
     y += 3
 print(tree_count)
 
-# PART 2 HINTS
-# Every single test has an instance of "1"
-# Whatever direction you need to go "1" in, make that i, as it cycles through your range in the for loop
-# Make y the other number
+
+# Right 1, Down 1
+y = 0
+tree_count2 = 0
+
+for i in range(len(content)):
+    if content[i][y] == '#':
+        tree_count2 += 1
+    y += 1
+print(tree_count2)
+
+# Right 5, Down 1
+y = 0
+tree_count3 = 0
+
+for i in range(len(content)):
+    if content[i][y] == '#':
+        tree_count3 += 1
+    y += 5
+print(tree_count3)
+
+# Right 7, Down 1
+y = 0
+tree_count4 = 0
+
+for i in range(len(content)):
+    if content[i][y] == '#':
+        tree_count4 += 1
+    y += 7
+print(tree_count4)
+
+# Right 1, Down 2
+# Use i % 2 != 0 (could use i == 0) to skip every other row
+y = 0
+tree_count5 = 0
+
+for i in range(len(content)):
+    if i % 2 != 0:
+        pass
+    else:
+        if content[i][y] == '#':
+            tree_count5 += 1
+        y += 1
+print(tree_count5)
+
+multiplied_trees = tree_count * tree_count2 * tree_count3 * tree_count4 * tree_count5
+print(f"The answer is {multiplied_trees}")
